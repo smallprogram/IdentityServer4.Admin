6667ccec4747087c88fc4c36ff96f5650e36cd0a
smallprogram <smallprogram@foxmail.com>
2019-05-10 00:15:54 +0800

    update, Angular Idp Client Add oidc、material、material-design-icons

--- SP.Idp/IdpClient/src/app/app.module.ts
+++ SP.Idp/IdpClient/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; //动画
+
+
+import 'hammerjs'; //Angular material 手势支持
+import { MaterialModule } from './shared/material.module'; //所有Angular material的组件
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
