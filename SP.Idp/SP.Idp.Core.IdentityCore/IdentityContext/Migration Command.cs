fb85a68d69464dfbfb984cb291094c006e3e8ef5
smallprogram <smallprogram@foxmail.com>
2019-05-14 11:39:31 +0800

    Rebulid, change lib to .NetCore2.2 Lib

---
+++ SP.Idp/SP.Idp.Core.IdentityCore/IdentityContext/Migration Command.cs
@@ -0,0 +1,30 @@
+﻿/// *************************************************************************************
+/// Asp.net Core Identity 迁移
+/// 
+/// 添加迁移
+/// add-migration InitAspNetCoreIdentity  -c IdentityCoreContext -o Migrations/AspNetCoreIdentity/AspNetCoreIdentityDB
+/// 删除迁移
+/// remove-migration -c IdentityCoreContext
+/// 更新数据库
+/// update-database -c IdentityCoreContext
+/// 删除数据库
+/// drop-database -c IdentityCoreContext
+/// 
+/// *************************************************************************************
+/// 
+
+
+///******************************************************************
+/// IdentityServer4 迁移
+/// 
+/// 
+/// 添加迁移
+/// add-migration InitIdentityServerPersistedGrant -c IdpPersistedGrantDbContext -o Migrations/IdentityServer/PersistedGrantDb
+/// add-migration InitIdentityServerConfiguration -c IdpConfigurationDbContext -o Migrations/IdentityServer/ConfigurationDb
+/// 更新数据库
+/// update-database -c IdpPersistedGrantDbContext
+/// update-database -c IdpConfigurationDbContext
+/// 
+/// 
+/// 
+///******************************************************************
---
+++ SP.Idp/SP.Idp.Resources/AccountResources/AccountOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SP.Idp.Resources.AccountResources
+{
+    public class AccountOptions
+    {
+        public static bool AllowLocalLogin = true;
+        public static bool AllowRememberLogin = true;
+        //记住登录的持续时间
+        public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
+        //是否显示登出页面
+        public static bool ShowLogoutPrompt = true;
+        //注销后是否自动重定向
+        public static bool AutomaticRedirectAfterSignOut = false;
+
+        // 指定正在使用的Windows身份验证方案
+        public static readonly string WindowsAuthenticationSchemeName = Microsoft.AspNetCore.Server.IISIntegration.IISDefaults.AuthenticationScheme;
+        // if user uses windows auth, should we load the groups from windows
+        public static bool IncludeWindowsGroups = false;
+
+        public static string InvalidCredentialsErrorMessage = "Invalid username or password";
+    }
+}
