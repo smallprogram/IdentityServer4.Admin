60de5617577ce278b54d6b85932cd45a6d0239ce
smallprogram <smallprogram@foxmail.com>
2019-06-06 10:24:18 +0800

    Update Idp Angular

--- SP.Idp/IdpClient/src/app/app.module.ts
+++ SP.Idp/IdpClient/src/app/app.module.ts
@@ -8,11 +8,17 @@ import { MaterialModule } from './shared/material.module'; //所有Angular mater
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/oidc/login/login.component';
+import { LogoutComponent } from './component/oidc/logout/logout.component';
+import { RedirectNewTokenComponent } from './component/oidc/redirect-new-token/redirect-new-token.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    RedirectNewTokenComponent
   ],
   imports: [
     BrowserModule,
---
+++ SP.Idp/IdpClient/src/app/component/oidc/redirect-new-token/redirect-new-token.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redirect-new-token',
+  templateUrl: './redirect-new-token.component.html',
+  styleUrls: ['./redirect-new-token.component.scss']
+})
+export class RedirectNewTokenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
